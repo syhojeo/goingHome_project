2cb817e50304a5064949e21ee60821a7b0c30e22
JONG HA JANG <dreamfinder2019@gmail.com>
2019-06-28 13:56:45 +0900

    initialized host_eventhandler, the first node for project

---
+++ jang_src/goinghome_host_eventhandler/src/main.cpp
@@ -0,0 +1,9 @@
+#include "ros/ros.h"
+
+int main(int argc, char **argv)
+{
+    ros:init(argc, argv, goinghome_host_eventhandler);
+    ros::Nodehandle nh;
+    ROS_INFO("Hello world!\n");
+    return 0;
+}
