7a4af8167a6e132b1f2eb42d244c0f11fb0ccc25
missjeong <ilovwjddk@naver.com>
2019-07-11 14:02:06 +0900

    face_test_1

---
+++ ROS_Study_python/face_test/sick_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+from face_test_2.srv import *
+
+def add_two_ints_client(x):
+    rospy.wait_for_service('test')
+    try:
+        add_two_ints = rospy.ServiceProxy('test', test)
+        resp1 = add_two_ints(x)
+        return resp1.id
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "%s [x]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        x = int(sys.argv[1])
+    else:
+        print usage()
+        sys.exit(1)
+    #print "Requesting %d"%(x)
+    print "Requesting {0}".format(x)
+    #print "%d = %d"%(x, add_two_ints_client(x))
+    print "{0} = {1}".format(x, add_two_ints_client(x))
